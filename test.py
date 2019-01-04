288a6d94f7ac802fd19a583b3432b6e9ae153aab
liqiduan <lee.nju@live.com>
2019-01-04 23:19:45 +0800

    Test: Make test better

--- test.py
+++ test.py
@@ -4,31 +4,23 @@ import unittest
 import duang
 
 class PareserHtml(unittest.TestCase):
+    def checkParse(self, context, result, pre='The', post='Chater'):
+        d = duang.Duang(pre, post)
+        r = d.parseIndex(context)
+        
+        for i in r.items():
+            self.assertEquals(r[i[0]], i[1])
 
     def testIndexToLink(self):
-        link = "test_href"
-        index = 1234
-        title = "The Title"
-
-        context = '<a href="%s">The %d Chater %s</a>' % (link, index, title)
-        d = duang.Duang()
-        r = d.parseIndex(context)
-        
-        self.assertEquals(r['link'], link)
-        self.assertEquals(r['index'], index)
-        self.assertEquals(r['title'], title)
+        context = '<a href="a_href">The 1234 Chater Hello world</a>' 
+        result = {'link':"a_href", 'index':1234, 'title':"Hello world"}
+        self.checkParse(context, result)
 
     def testIndexToLinkCN(self):
-        link = r"/8/8208/10024409.html"
-        index = 1093
-        title = r'大结局'
+        result = { 'link' : r"/8/8208/10024409.html",
+                  'index' : 1093, 'title' : r'大结局'}
         context = r'<dd><a href ="/8/8208/10024409.html">第1093章大结局</a></dd>'
-        d = duang.Duang(pre = r'第', post = r'章')
-        r = d.parseIndex(context)
-        
-        self.assertEquals(r['link'], link)
-        self.assertEquals(r['index'], index)
-        self.assertEquals(r['title'], title)
+        self.checkParse(context, result, pre = r'第', post = r'章')
 
 if __name__=='__main__':
     unittest.main()
