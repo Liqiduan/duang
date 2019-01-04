62f48b60871baa2e88ef82c265471a5da41857d8
liqiduan <lee.nju@live.com>
2019-01-04 22:53:19 +0800

    Test: Add chinese test case

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+#coding=utf8
 import unittest
 
 import duang
@@ -11,6 +12,18 @@ class PareserHtml(unittest.TestCase):
 
         context = '<a href="%s">The %d Chater %s</a>' % (link, index, title)
         d = duang.Duang()
+        r = d.parseIndex(context)
+        
+        self.assertEquals(r['link'], link)
+        self.assertEquals(r['index'], index)
+        self.assertEquals(r['title'], title)
+
+    def testIndexToLinkCN(self):
+        link = r"/8/8208/10024409.html"
+        index = 1093
+        title = r'大结局'
+        context = r'<dd><a href ="/8/8208/10024409.html">第1093章大结局</a></dd>'
+        d = duang.Duang(pre = r'第', post = r'章')
         r = d.parseIndex(context)
         
         self.assertEquals(r['link'], link)
